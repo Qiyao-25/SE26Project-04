a5534595a5b07909ec44f333ed13e231500f142d
Qiyao-25 <2489397324@qq.com>
2026-07-11 14:58:33 +0800

    iteration1

--- 界面原型迭代/src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useApp } from '../context/AppContext';
-
-export default function ProtectedRoute() {
-  const { loggedIn } = useApp();
-  const location = useLocation();
-  if (!loggedIn) return <Navigate to="/login" replace state={{ from: location }} />;
-  return <Outlet />;
-}
